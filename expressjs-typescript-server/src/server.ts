c45ec1ef69aa40d8a06ae46711407743eaf20233
udoykumar <udoypal.kumar2002@gmail.com>
2026-07-27 21:02:30 +0600

    post request andd body data recevied postgres neon db connection

--- expressjs-typescript-server/src/server.ts
+++ expressjs-typescript-server/src/server.ts
@@ -1,9 +1,36 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+const app: Application = express();
 const port = 5000;
+// middleware
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_tv7ylGEc0VmP@ep-icy-water-av9877d9-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+app.get("/", (req: Request, res: Response) => {
+  // res.send("Hello World!!!!");
+
+  res.status(200).json({
+    message: "hello world!!",
+    author: "next level",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "created post request",
+    data: { name, email },
+  });
 });
 
 app.listen(port, () => {
