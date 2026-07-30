3eaf85e0d48959d5f6a78e08c409e58abb94a5e8
udoykumar <udoypal.kumar2002@gmail.com>
2026-07-30 11:25:34 +0600

    database connect and table create

--- expressjs-typescript-server/src/server.ts
+++ expressjs-typescript-server/src/server.ts
@@ -15,6 +15,27 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_tv7ylGEc0VmP@ep-icy-water-av9877d9-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY, 
+      name VARCHAR (20),
+      email VARCHAR (20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT, 
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+    console.log("table create successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   // res.send("Hello World!!!!");
 
