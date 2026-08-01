645d513f13c86af5aa015020decb619d6f7dd458
udoykumar <udoypal.kumar2002@gmail.com>
2026-08-01 16:54:04 +0600

    create profile db table schema

--- expressjs-typescript-server/src/db/index.ts
+++ expressjs-typescript-server/src/db/index.ts
@@ -19,6 +19,21 @@ export const initDB = async () => {
       updated_at TIMESTAMP DEFAULT NOW()
       )
       `);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS profiles(
+        id  SERIAL PRIMARY KEY,
+        user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+
+        bio TEXT,
+        address TEXT,
+        phone VARCHAR(15),
+        gender VARCHAR(10),
+        
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
     console.log("table create successfully");
   } catch (error) {
     console.log(error);
